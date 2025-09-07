f621d6f208f657ab34457a9aba2380812ba74c33
Oliver <oliver.laura@ucb.edu.bo>
2025-09-08 08:47:53 +0900

    init git

---
+++ src/tarija.js
@@ -0,0 +1,12 @@
+class Estacionamiento{
+    constructor(){
+        this.horaIngreso = null;
+    }
+    /* horaIngresoVehiculo(hora){
+        return this.horaIngreso=hora;
+    }*/
+
+}
+
+
+module.exports = Estacionamiento;

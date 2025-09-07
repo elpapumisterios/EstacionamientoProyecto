a45fddce4d52c27f2a23a1f2cb23858dc2ea92a0
Oliver <oliver.laura@ucb.edu.bo>
2025-09-08 08:59:53 +0900

    jest: Hora salida vehiculo

--- src/tarija.js
+++ src/tarija.js
@@ -1,9 +1,13 @@
 class Estacionamiento{
     constructor(){
         this.horaIngreso = null;
+        this.horaSalida = null;
     }
      horaIngresoVehiculo(hora){
         return this.horaIngreso=hora;
+    }
+    horaSalidaVehiculo(hora){
+        return this.horaSalida=hora;    
     }
 
 }
--- src/tarija.test.js
+++ src/tarija.test.js
@@ -6,3 +6,9 @@ test("Registrar Hora de ingreso", () => {
         estacionamiento.horaIngresoVehiculo("08:00");
         expect(estacionamiento.horaIngreso).toBe("08:00");
 });
+test("Registrar hora de salida", () => {           
+    const estacionamiento= new Estacionamiento();
+    estacionamiento.horaIngresoVehiculo("08:00");
+    estacionamiento.horaSalidaVehiculo("10:00");
+    expect(estacionamiento.horaSalida).toBe("10:00");
+}   );
